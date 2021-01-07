16528584313de0f6724a2df8bf5b05c7b8b79373
Arkanipro <Arkanipro@gmail.com>
2021-01-08 00:01:32 +0100

    fixed Graph implementation

--- src/GraphImpl.java
+++ src/GraphImpl.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class GraphImpl implements Graph{
     public int[][] graph;
     public GraphImpl(int[][] ints) {
@@ -16,21 +18,38 @@ public class GraphImpl implements Graph{
 
     @Override
     public int succ(int v, int i) {
-        return v+i;
+        return graph[v][i];//einfahc v+i zu returnen wäre einfach den v+i ten Knoten zurückgeben und nicht den i ten Nachfolger
     }
 
     @Override
     public Graph transpose() {
-        for (int l = graph.length-1;l>=0;l--) {
+        ArrayList<Integer>[] arrayLists= new ArrayList[graph.length];
+        for(int i=0;i<graph.length;i++){
+            arrayLists[i]=new ArrayList<>();
+        }
+        for(int i=0;i<graph.length;i++){
+            for(int j=0;j<graph[i].length;j++){
+                arrayLists[graph[i][j]].add(i);
+            }
+        }
+        int[][] newg=new int[graph.length][];
+        for(int i=0;i<newg.length;i++){
+            newg[i]=new int[arrayLists[i].size()];
+            for(int j=0;j<arrayLists[i].size();j++){
+                newg[i][j]=arrayLists[i].get(j);
+            }
+        }
+        /*for (int l = graph.length-1;l>=0;l--) {
             if (graph[l].length == 0) {
                 continue;
             }
             Knotenswitch(l);
         }
-        return null;
+        return null;*/
+        return new GraphImpl(newg);
     }
 
-    public void Knotenswitch(int x) {
+    /*public void Knotenswitch(int x) {
         int deg = graph[x].length;
         if (deg != 2) {
             return;
@@ -39,5 +58,5 @@ public class GraphImpl implements Graph{
         graph[x][0] = graph[x][1];
         graph[x][1] = temp;
         return;
-    }
+    }*/
 }
--- src/ZusatzMain.java
+++ src/ZusatzMain.java
@@ -4,31 +4,30 @@ public class ZusatzMain {
                 { 1, 2 },	// Knoten 0 hat als Nachfolger Knoten 1 und 2.
                 { },	// Knoten 1 hat keine Nachfolger.
                 { 2 },	// Knoten 2 hat als Nachfolger sich selbst.
-                { 3,4 },
-                {4   }
         });
 
         System.out.println("Size: "+g.size());
         System.out.println("Deg von 0: "+g.deg(0));
         System.out.println("Deg von 1: "+g.deg(1));
         System.out.println("Deg von 2: "+g.deg(2));
-        System.out.println("2 Nachfolger von 0: "+g.succ(0,2));
+        System.out.println("2 Nachfolger von 0: "+g.succ(0,1));
 
         printGraph(g);
-        g.transpose();
-        printGraph(g);
+        Graph g2=g.transpose();
+        printGraph((GraphImpl) g2);
     }
 
     public static void printGraph(GraphImpl g) {
-        System.out.println("________________");
+        System.out.println("_________________☺");
         System.out.println("Graph: ");
         for (int i=0;i<g.graph.length;i++) {
             System.out.print("Knoten "+i+": ");
             for (int j=0;j<g.graph[i].length;j++) {
                 System.out.print(g.graph[i][j]+" ");
             }
+            System.out.println();
         }
-        System.out.println("__________________");
+        System.out.println("_________________☻");
     }
     static void printKonten(int indx,String parent,GraphImpl g) {
         System.out.println(" "+parent+indx);
